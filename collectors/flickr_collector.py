01ebf43e4c26a89a466d1f82b29c2a2b0d1833e7
Or Nachmias <or.nachmias@gmail.com>
2019-08-30 10:19:37 +0300

    Initial separation to classes

--- collectors/flickr_collector.py
+++
@@ -1,6 +0,0 @@
-from collectors.base_collector import BaseCollector
-
-
-class FlickrCollector(BaseCollector):
-    def collect(self):
-        pass
--- collectors/twitter_collector.py
+++
@@ -1,6 +0,0 @@
-from collectors.base_collector import BaseCollector
-
-
-class TwitterCollector(BaseCollector):
-    def collect(self):
-        pass
