55c722abd2bcf55371bae5021cac4ef55b38a580
mgilead <mgilead@apple.com>
2019-09-06 12:30:16 +0300

    Fixed everything

--- configurations.py
+++ configurations.py
@@ -1,6 +1,9 @@
-images_directory = "./data/b-t4sa_imgs"
-images_classification_train_file = "./data/b-t4sa_imgs/b-t4sa_train.txt"
-images_classification_eval_file = "./data/b-t4sa_imgs/b-t4sa_val.txt"
-descriptions_file = "./data/raw_tweets_text.csv"
+import os
+
+data_path = os.path.join(os.path.dirname(__file__), "data")
+images_directory = os.path.join(data_path, "b-t4sa_imgs")
+images_classification_train_file = os.path.join(images_directory, "b-t4sa_train.txt")
+images_classification_eval_file = os.path.join(images_directory, "b-t4sa_val.txt")
+descriptions_file = os.path.join(images_directory, "raw_tweets_text.csv")
 logs_dir = "./logs"
 image_size = 224
